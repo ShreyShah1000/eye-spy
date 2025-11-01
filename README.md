0e8d5991e3af7f7b05362c7ca4781ec6a54f6d10
Maple <bigmmushroomfungus@gmail.com>
2025-11-01 12:32:25 -0400

    Co-authored-by: ShreyShah1000 <ShreyShah1000@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+### ABOUT
+Lookit is an interactive learning app that revolutionizes
+
+### TO DO
+1. Make basic functionality
+    a. Camera preview
+        i. AI chooses an object
+        ii. AI generates a prompt (i.e. "I spy something red, shiny and rhymes with 'bapple'")
+    b. AI recognition
+    c. UI/UX
+    d. "Game loop" functionality
---
+++ backend/app.py
@@ -0,0 +1,48 @@
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+import requests, json, os, base64
+
+# app = Flask(__name__)
+# CORS(app)
+
+API_KEY=os.getenv("OPENROUTER_API_KEY")
+MODEL="meta-llama/llama-guard-4-12b"
+URL = "https://openrouter.ai/api/v1/chat/completions"
+
+example_image = "https://pixabay.com/photos/nature-waters-lake-island-3082832/"
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            # first send your text question/prompt
+            {"type": "text", "text": "Treat this image as an eye spy game. Your job is to choose an object from the image. Difficulty is easy"},
+            # then the image
+            {"type": "image_url", "image_url": {"url": example_image}}
+        ]
+    }
+]
+
+payload = {
+    "model": MODEL,
+    "messages": messages,
+    "temperature": 0.0,
+    "max_tokens": 300
+}
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(URL, headers=headers, json=payload)
+response.raise_for_status()
+result = response.json()
+print(json.dumps(result, indent=2))
+
+# @app.route("/processImage", methods=["POST"])
+# def processImage():
+#     return 0
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
