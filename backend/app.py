b6e2470c342fbf24830aa07dc0747594bc147a74
Shrey Shah <shahshrey518@gmail.com>
2025-11-02 01:38:31 -0400

    backend ready to deploy

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import requests, json, os, base64
+import requests, json, os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -139,4 +139,4 @@ def game_check():
     
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
